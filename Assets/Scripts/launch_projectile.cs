056f6aba64fa52a33be5e8f05d62c31696716cdf
Juan <juan.carosella@gmail.com>
2022-11-22 14:42:07 -0300

    fix

--- Assets/Scripts/launch_projectile.cs
+++ Assets/Scripts/launch_projectile.cs
@@ -15,6 +15,9 @@ public class launch_projectile : MonoBehaviour
     {
         GameObject ball = Instantiate(projectile, transform.position,  
                                                      transform.rotation);
+           BoxCollider gameObjectsBoxCollider= ball.AddComponent<BoxCollider>();
+           gameObjectsBoxCollider.size = new Vector3(1f, 1.311829f, 1.208954f);
+           gameObjectsBoxCollider.center = new Vector3(0f, 0.6176605f, 0.004940033f);
            ball.GetComponent<launch_projectile>().enabled = false;
            ball.GetComponent<Rigidbody>().AddRelativeForce(new Vector3 
                                                 (1000, launchVelocity,0));
